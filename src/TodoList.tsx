a649c8de8ea97dfe1fe8524ea783f4be45fbcf2c
Jess Martin <jessmartin@gmail.com>
2023-07-11 16:58:25 -0400

    Move invites to top right

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -28,105 +28,107 @@ export const TodoList = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <button
-        className="p-2"
+        className="float-right bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-2 mr-2"
         onClick={() => shell.setLayout(ShellLayout.DEVICE_INVITATIONS)}
       >
-        Device Invitations
+        Invite
       </button>
-      <h1 className="text-3xl font-bold leading-tight text-gray-900 mb-2">
-        Todo List
-      </h1>
-      {todos && (
-        <ul>
-          {todos.map((task, index) => (
-            <li
-              key={index}
-              className="flex items-center text-gray-700 mb-1"
-              onMouseOver={() => {
-                setShowDeleteTask(index);
-              }}
-              onMouseLeave={() => {
-                setShowDeleteTask(null);
-              }}
-            >
-              <input
-                className="mr-2 rounded shadow hover:pointer-cursor"
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => (task.completed = !task.completed)}
-              />
-              <span
-                className="hover:pointer-cursor"
-                onClick={() => {
-                  setEditingTask(index);
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-3xl font-bold leading-tight text-gray-900 mb-2">
+          Todo List
+        </h1>
+        {todos && (
+          <ul>
+            {todos.map((task, index) => (
+              <li
+                key={index}
+                className="flex items-center text-gray-700 mb-1"
+                onMouseOver={() => {
+                  setShowDeleteTask(index);
+                }}
+                onMouseLeave={() => {
+                  setShowDeleteTask(null);
                 }}
               >
-                {editingTask === index ? (
-                  <span>
-                    <input
-                      className="mr-2 rounded shadow p-0 px-2"
-                      type="text"
-                      value={task.title}
-                      onChange={(e) => {
-                        task.title = e.target.value;
-                      }}
-                      onKeyUp={(e) => {
-                        if (e.key === "Enter") {
+                <input
+                  className="mr-2 rounded shadow hover:pointer-cursor"
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={() => (task.completed = !task.completed)}
+                />
+                <span
+                  className="hover:pointer-cursor"
+                  onClick={() => {
+                    setEditingTask(index);
+                  }}
+                >
+                  {editingTask === index ? (
+                    <span>
+                      <input
+                        className="mr-2 rounded shadow p-0 px-2"
+                        type="text"
+                        value={task.title}
+                        onChange={(e) => {
+                          task.title = e.target.value;
+                        }}
+                        onKeyUp={(e) => {
+                          if (e.key === "Enter") {
+                            setEditingTask(null);
+                          }
+                        }}
+                      />
+                      <button
+                        className="mr-2 bg-white rounded shadow p-0 px-2 border border-gray-400"
+                        onClick={(e) => {
+                          e.stopPropagation();
                           setEditingTask(null);
-                        }
-                      }}
-                    />
+                        }}
+                      >
+                        Done
+                      </button>
+                    </span>
+                  ) : (
+                    task.title
+                  )}
+                  {showDeleteTask === index && editingTask !== index && (
                     <button
-                      className="mr-2 bg-white rounded shadow p-0 px-2 border border-gray-400"
+                      className="bg-white rounded ml-2 p-0 px-2 hover:bg-gray-100 hover:cursor-pointer"
                       onClick={(e) => {
                         e.stopPropagation();
-                        setEditingTask(null);
+                        space.db.remove(task);
                       }}
                     >
-                      Done
+                      x
                     </button>
-                  </span>
-                ) : (
-                  task.title
-                )}
-                {showDeleteTask === index && editingTask !== index && (
-                  <button
-                    className="bg-white rounded ml-2 p-0 px-2 hover:bg-gray-100 hover:cursor-pointer"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      space.db.remove(task);
-                    }}
-                  >
-                    x
-                  </button>
-                )}
-              </span>
-            </li>
-          ))}
-        </ul>
-      )}
-      <div className="mt-1">
-        <input
-          className="mr-2 rounded shadow"
-          type="text"
-          value={newTodoTitle}
-          onChange={(e) => {
-            setNewTodoTitle(e.target.value);
-          }}
-          onKeyUp={(e) => {
-            if (e.key === "Enter") {
-              handleNewTodo();
-            }
-          }}
-        />
-        <button
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-          onClick={handleNewTodo}
-        >
-          Add Todo
-        </button>
+                  )}
+                </span>
+              </li>
+            ))}
+          </ul>
+        )}
+        <div className="mt-1">
+          <input
+            className="mr-2 rounded shadow"
+            type="text"
+            value={newTodoTitle}
+            onChange={(e) => {
+              setNewTodoTitle(e.target.value);
+            }}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                handleNewTodo();
+              }
+            }}
+          />
+          <button
+            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            onClick={handleNewTodo}
+          >
+            Add Todo
+          </button>
+        </div>
       </div>
     </div>
   );
